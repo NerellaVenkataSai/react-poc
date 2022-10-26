874eea87a48cea18d6b9dd75e9a195047f9372c6
NerellaVenkataSai <84185688+NerellaVenkataSai@users.noreply.github.com>
2022-10-26 22:35:32 +0530

    header

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { AppBar, Box, Toolbar, Typography, Button } from '@mui/material';
+import LogoutIcon from '@mui/icons-material/Logout';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,6 +12,16 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <Box sx={{ flexGrow: 1, backgroundColor: 'grey' }}>
+      <AppBar position="fixed" className="header-main">
+        <Toolbar>
+          <Button color="inherit">My Logo</Button>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} />
+          <AccountCircleOutlinedIcon sx={{ mr: '8px'}} />
+          <LogoutIcon />
+        </Toolbar>
+      </AppBar>
+    </Box>
     <App />
   </React.StrictMode>
 );
